e89c047dad80e3de5357b3fdb44ceb9ba7199a6a
thehambeard <twarehamjr@gmail.com>
2020-09-15 23:40:56 -0400

    Initial

---
+++ TBTest/ActionBarFix.cs
@@ -0,0 +1,31 @@
+﻿using Kingmaker;
+using Kingmaker.Blueprints;
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.UI.Common;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TBTest
+{
+    public class ActionBarFix
+    {
+        private static UnitEntityData spawn;
+
+        public UnitEntityData CSpawn(string guid)
+        {
+            UnitEntityData value = Game.Instance.Player.MainCharacter.Value;
+            spawn = Game.Instance.EntityCreator.SpawnUnit(ResourcesLibrary.TryGetBlueprint<BlueprintUnit>(guid), value.Position, Quaternion.LookRotation(value.OrientationDirection), Game.Instance.CurrentScene.MainState);
+            spawn.Descriptor.SwitchFactions(Game.Instance.BlueprintRoot.PlayerFaction, true);
+            return spawn;
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(UIUtility), "GetGroup")]
+        internal static class UIUtility_GetGroup_Patch
+        {
+            private static void Postfix(ref List<UnitEntityData> __result)
+            {
+                __result.Add(spawn);
+            }
+        }
+    }
+}
---
+++ TBTest/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace TBTest
+{
+#if (DEBUG)
+    [EnableReloading]
+#endif
+    internal static class Main
+    {
+
+        private static ActionBarFix abf = new ActionBarFix();
+        private static string text = "";
+
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(Main.OnToggle);
+            Main.Logger = modEntry.Logger;
+            new HarmonyLib.Harmony(modEntry.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
+            modEntry.OnGUI = OnGUI;
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Main.enabled = value;
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            text = GUILayout.TextField(text);
+            if (GUILayout.Button("Custom Spawn"))
+            {
+                abf.CSpawn(text);
+            }
+        }
+
+        public static bool enabled;
+
+        public static UnityModManager.ModEntry.ModLogger Logger;
+    }
+}
